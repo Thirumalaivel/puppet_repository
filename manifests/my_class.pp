e8a08d7ad4bc56889d813d76796e15bcbfc59de1
thirumalaivel <thirumalaivel7@gmail.com>
2020-02-05 17:37:47 +0530

    Production branch

--- manifests/my_class.pp
+++
@@ -1,4 +0,0 @@
-class my_class {
-  notice("operating system is: ${::os["name"]}")
-  notice("Environment is: lookup{"message"}")
-}
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,17 @@
 
 ## site.pp ##
+class test {
+  $app = lookup('message')
+  notice("operating system is: ${::timezone}")
+  notice("Environment is: $app")
+  file { '/tmp/testing':
+	ensure  => file,
+  }
+}
+
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
-  class { 'my_class': }
+  class { 'test': }
   
 }
